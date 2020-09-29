e6c94912cd6e0170186646aa7fd9617a2482541b
Amanda D'Argenio <dargenioa14@gmail.com>
2020-09-28 20:36:15 -0400

    concat arrays and generate random character

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-let characters = {
-["!", "@", "#", "$", "%", "&", "*", "(", ")", "_", "+"],
-lowerCaseLetters: ["a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
-upperCaseLetters: ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-numbers: ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"]
-};
+let specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
+let lowerCaseLetters = ["a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
+let upperCaseLetters = ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let returnVal = [];
+let newPassword;
 
-generatePassword = () => {
+function generatePassword () {
   let length = parseInt(
     prompt("How many characters would you like your password to contain?")
   );
@@ -21,40 +19,58 @@ generatePassword = () => {
   };
 
   let confirms = {
-    special: {
-      question: confirm('Would you like your password to contain special characters?'),
-      characters: 
-    }
-    
+    special: confirm('Would you like your password to contain special characters?'),
     upperCase: confirm('Would you like you password to contain uppercase letters?'),
     lowerCase: confirm('Would you like your password to contain lowercase letters?'),
-    numbers: confirm('Would you like your password to contain numbers?')
+    numbers: confirm('Would you like your password to contain numbers?'),
   };
 
-if  ((confirms.special) || confirms.upperCase || confirms.lowerCase || confirms.numbers) {
-  alert("Please wait while while your password is generated.");
+  if ((confirms.special) || (confirms.upperCase) || (confirms.lowerCase) || (confirms.numbers)) {
+    alert("Please wait while while your password is generated.");
+  } else {
+    alert('Please choose at least one character type.');
+    return generatePassword();
+  };
+
+  if (confirms.special) {
+    returnVal = returnVal.concat(specialCharacters);
+  };
+
+  if (confirms.upperCase) {
+    returnVal = returnVal.concat(upperCaseLetters);
+  };
+
+  if (confirms.lowerCase) {
+    returnVal = returnVal.concat(lowerCaseLetters);
+  };
+
+  if (confirms.numbers) {
+    returnVal = returnVal.concat(numbers);
+  };
+
+  console.log(returnVal);
+
+  for (let i = 0; i < length; i++) {
+    random = Math.floor(Math.random() * returnVal.length);
+    newPassword = returnVal[random];
+    console.log(newPassword);
   }
 
-} else {
-  alert('Please choose at least one character type.');
-  return generatePassword();
 };
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-
-
-};
-
-generatePassword();
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
